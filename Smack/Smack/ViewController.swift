6b4a51537d8208f0fe398db64f78e7605e9ba513
Elyess Mahmoud <elyessmahmoud@Elyesss-MacBook-Pro.local>
2019-09-04 12:07:45 +0100

    init

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Elyess Mahmoud on 9/4/19.
+//  Copyright © 2019 Elyess Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
